47af73c453db5b37563fc89a48df582b994ada12
Orokh <guillaume.kaiffer@gmail.com>
2023-06-30 17:51:28 +0200

    fix: remove previous config

--- nvim/.config/nvim/lua/colors.lua
+++
@@ -1,9 +0,0 @@
-require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    no_italic = true,
-})
-
-vim.cmd.colorscheme "catppuccin"
-
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
